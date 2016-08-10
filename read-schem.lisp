59b7eed20595027359371e96417c42ad131af285
Philipp Dikmann <mail@philippdikmann.de>
2016-08-10 07:46:02 +0200

    add feature: parse file, lines into schematic

--- read-schem.lisp
+++ read-schem.lisp
@@ -1,40 +1,38 @@
-(defvar *lines* '())
-
-(defun zener ()
+(defun read-file (file-name)
   (with-open-file
-      (f "zener.net")
-    (labels
-        ((keep-reading ()
-           (let ((line (read-line f nil nil) ; return nil on eof
-                   ))
-             (when line
-               (format t "~s~c" line #\newline)
-               (unless (some #'(lambda (c) (char= (elt line 0) c))
-                             '(#\. #\*))
-                 ;; (or (char= (elt line 0) #\.)
-                 ;;     (char= (elt line 0) #\*))
-                 (setq *lines* (cons line *lines*)))
-               (keep-reading)))))
-      (keep-reading))))
+      (f file-name)
+    (let ((lines nil))
+      (labels
+          ((keep-reading ()
+             (let ((line (read-line f nil nil) ; return nil on eof
+                     ))
+               (if line
+                   (progn
+                     ;;(format t "~s~c" line #\newline)
+                     (unless (some #'(lambda (c) (char= (elt line 0) c))
+                                   '(#\. #\*)) ; ignore lines starting with . or *
+                       (setq lines (cons line lines)))
+                     (keep-reading))
+                   (nreverse lines)))))
+        (keep-reading)))))
 
-(defun split-string (src
-                     delim
-                     &optional
-                       (count 0)
-                       (accum '()))
-  "split string into separate words"
-  (cond
-    ((= count (length src))
-     (nreverse (cons src accum)))
-    ((char= (elt src count) delim)
-     (split-string (subseq src (1+ count))
-                   delim
-                   0
-                   (cons (subseq src 0 count) accum)))
-    (t
-     (split-string src delim (1+ count) accum))))
+(defun parse-line (line)
+  (let ((tokens (split-string line #\space)))
+    ;; todo: anticipate more than 2 connected nets
+    (multiple-value-bind (name net1 net2) (apply #'values tokens)
+      (add-component :name name
+                     :pins (list (make-pin :name "0" :net net1)
+                                 (make-pin :name "1" :net net2)))
+      (add-net :name net1)
+      (add-net :name net2))))
 
-(defun split-string/2 (src delim)
+(defun parse-file (file-name)
+  (setq *schematic* (make-schematic))
+  (let ((lines (read-file file-name)))
+    (mapcar #'parse-line lines))
+  *schematic*)
+
+(defun split-string (src delim)
   "split string into separate words"
   (labels ((recur (src delim count accum)
              (cond
@@ -49,3 +47,31 @@
                 (recur src delim (1+ count) accum)))))
     (recur (string-trim (list delim) src)
            delim 0 nil)))
+
+(defstruct pin
+  (name "PIN-NAME" :type string)
+  (net "PIN-NET" :type string))
+
+(defstruct component
+  (name "NONAME" :type string)
+  (pins '()))
+
+(defstruct net
+  (name "NET-NAME" :type string))
+
+(defstruct schematic
+  (components '())
+  (nets '()))
+
+(defparameter *schematic* (make-schematic))
+
+(defun add-component (&key name pins)
+  (setf (schematic-components *schematic*)
+        (cons (make-component :name name
+                              :pins pins)
+              (schematic-components *schematic*))))
+
+(defun add-net (&key name)
+  (setf (schematic-nets *schematic*)
+        (cons (make-net :name name)
+              (schematic-nets *schematic*))))
